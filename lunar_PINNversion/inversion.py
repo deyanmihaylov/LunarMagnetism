564cfb1c61c2e0905bf332e99ba6b4e5ea3446b3
Deyan Mihaylov <d.p.mihaylov@gmail.com>
2026-01-24 01:18:45 -0500

    Fix linting

--- lunar_PINNversion/inversion.py
+++ lunar_PINNversion/inversion.py
@@ -9,14 +9,30 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
 
 # Create data loaders
-train_colloc_loader, train_boundary_loader = create_dataloaders(batch_size=8196)
+(
+    train_colloc_loader,
+    train_boundary_loader,
+) = create_dataloaders(batch_size=8196)
 
 wandb.init(project='lunarmagnetism')
 
 # Initialize model
-model = BVectNetFourierSIREN(in_dim=3, out_dim=1, hidden_layers=[256, 256, 256, 256],
-                             device=device, num_frequencies=2)
+model = BVectNetFourierSIREN(
+    in_dim=3,
+    out_dim=1,
+    hidden_layers=[256, 256, 256, 256],
+    device=device,
+    num_frequencies=2,
+)
 
 # Train the model
-model.train_model(train_colloc_loader, train_boundary_loader, train_colloc_loader, train_boundary_loader,
-                  lambda_pde=0, lambda_bc=1, epochs=500, lr=1e-3)
+model.train_model(
+    train_colloc_loader,
+    train_boundary_loader,
+    train_colloc_loader,
+    train_boundary_loader,
+    lambda_pde=0,
+    lambda_bc=1,
+    epochs=500,
+    lr=1e-3,
+)
